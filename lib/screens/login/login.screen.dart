4c7d8544cb2e6f6886311114ce40fe228039d9c1
dao <adolfo_24@yahoo.com>
2022-06-23 17:41:34 -0600

    Codigo base

---
+++ lib/screens/login/login.screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+enum Gender {
+  Email,
+  password,
+}
+
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(context) =>
+      Scaffold(body: LayoutBuilder(builder: (context, constraints) {
+        return AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            color: Colors.lightGreen[200],
+            padding: constraints.maxWidth < 500
+                ? EdgeInsets.zero
+                : const EdgeInsets.all(30.0),
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 30.0, horizontal: 25.0),
+                constraints: BoxConstraints(
+                  maxWidth: 500,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text("Iniciar Sesión"),
+                      TextField(
+                          decoration: InputDecoration(labelText: "Usuario")),
+                      TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(labelText: "Password")),
+                      RaisedButton(
+                          color: Colors.blue,
+                          child: Text("Iniciar",
+                              style: TextStyle(color: Colors.white)),
+                          onPressed: () {})
+                    ]),
+              ),
+            ));
+      }));
+}
