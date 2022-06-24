8cd86e0cb880ffe80ff19e215eeafd0f2ced3cdc
Diego Garcia <diegofabriciogarcia@gmail.com>
2022-06-23 18:04:10 -0600

    Cambio en el titulo de la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Soft',
+      title: 'El mejor frot-end',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
