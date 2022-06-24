385926cd83a30bebe45549be8f2894343e848aba
ErickReyes19 <erickjosepineda33@gmail.com>
2022-06-23 22:49:04 -0600

    El mejor login

--- lib/screens/home/home.screen.dart
+++ lib/screens/home/home.screen.dart
@@ -27,7 +27,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Pantalla Login mamalona:',
             ),
             Text(
               '$_counter',
