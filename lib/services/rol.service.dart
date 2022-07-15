3b88d17554aee47ce904b170b9ba27f61643d806
henrryJavinu1 <henryjavinu1@gmail.com>
2022-07-14 22:09:47 -0600

    correcciones rol y permisos
    correciones en front de los services y controler de rol y permiso

--- lib/services/rol.service.dart
+++ lib/services/rol.service.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:developer';
+import '../models/rol.model.dart';
+import 'package:soft_frontend/constans.dart';
 
-Future<void> crearRol(String rol, String descripcion, context) async {
-  if (rol.isNotEmpty && descripcion.isNotEmpty) {
-    var response = await http.post(
-        Uri.parse("http://localhost:8080/api/permiso/crearol"),
-        body: ({'rol': rol, 'descripcion': descripcion}));
+Future<Rol?> crearRol(String rol, String descripcion) async {
+  var client = http.Client();
+  Rol? rol = null;
+  try {
+    var response = await client.post(Uri.parse(API_URL + "roles/crearol"),
+        body: {'rol': rol, 'descripcion': descripcion});
 
     if (response.statusCode == 200) {
       ScaffoldMessenger.of(context)
           .showSnackBar(const SnackBar(content: Text("rol Creado")));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("Error al crear rol")));
     }
-  } else {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(const SnackBar(content: Text("Error al crear rol")));
+  } finally {
+    client.close();
   }
 }
