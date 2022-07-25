b4d214fbc1085d151f72a407d07ee39252d412a6
Willian Josue <wjoe1995@live.com>
2022-07-24 22:19:55 -0600

    actualizacion Arqueo

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: 'login',
+      initialRoute: 'traer_arqueo',
       routes: {
         'login': (_) => const Login(),
         'pantalla_principal': (_) => const PantallaPrincipal(),
