cc9bc1121e88381b667d6e4cb1e53fa002348ba6
Jhonatan Josue Osorto Centeno <jhonatanosorto39@gmail.com>
2022-07-26 13:50:00 -0600

    cambiando ruta inicial y cambios en controller cliente

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: 'prueba',
+      initialRoute: 'login',
       routes: {
         'login': (_) => const Login(),
 
