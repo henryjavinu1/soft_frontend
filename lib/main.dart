e9c30e2d032a49a3d1c9c32b68dcc2183da07d5c
Diego Garcia <diegofabriciogarcia@gmail.com>
2022-08-02 14:11:44 -0600

    Corrección de ruta inicial

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: 'mostrar_ventas',
+      initialRoute: 'login',
       routes: {
         'login': (_) => const Login(),
 
