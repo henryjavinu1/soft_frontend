26eaa821ef4bf59e2a64edbefd914f719ba166c6
Asmodeus9563 <wjoe1995@live.com>
2022-08-14 17:02:20 -0600

    correccion de error en ruta de controller tipoproducto

--- lib/controllers/tipoproducto.controller.dart
+++ lib/controllers/tipoproducto.controller.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:soft_frontend/models/tipoproducto.model.dart';
+import 'package:soft_frontend/models/Tipoproducto.model.dart';
 import '../services/tipoproducto.service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
@@ -7,16 +7,13 @@ import 'package:soft_frontend/screens/tipoproducto/tipoproducto.screen.dart';
 import '../screens/tipoproducto/tipoproducto.screen.dart' as global;
 
 var estaCorrecto;
-bool controladorCrearTipoProducto(
-    String tipoproducto,
-    String descripcionProducto,
-    String isvTipoProducto,
-    context
-    ){
+bool controladorCrearTipoProducto(String tipoproducto,
+    String descripcionProducto, String isvTipoProducto, context) {
   bool funciona = false;
   Tipoproducto tipoProducto = Tipoproducto();
-  Future <Tipoproducto?>  producto2 = crearTipoProductoController(tipoproducto, descripcionProducto, isvTipoProducto, context);
-  if (producto2 != null){
+  Future<Tipoproducto?> producto2 = crearTipoProductoController(
+      tipoproducto, descripcionProducto, isvTipoProducto, context);
+  if (producto2 != null) {
     funciona = true;
   } else {
     funciona = false;
@@ -24,17 +21,13 @@ bool controladorCrearTipoProducto(
   return funciona;
 }
 
-bool controladorActualizarTipoProducto(
-    String id,
-    String tipoproducto,
-    String descripcionProducto,
-    String isvTipoProducto,
-    context
-    ){
+bool controladorActualizarTipoProducto(String id, String tipoproducto,
+    String descripcionProducto, String isvTipoProducto, context) {
   bool funciona = false;
   Tipoproducto tipoProducto = Tipoproducto();
-  Future <Tipoproducto?>  producto2 = actualizarTipoProductoController(id, tipoproducto, descripcionProducto, isvTipoProducto, context);
-  if (producto2 == null){
+  Future<Tipoproducto?> producto2 = actualizarTipoProductoController(
+      id, tipoproducto, descripcionProducto, isvTipoProducto, context);
+  if (producto2 == null) {
     funciona = true;
   } else {
     funciona = false;
@@ -42,17 +35,15 @@ bool controladorActualizarTipoProducto(
   return funciona;
 }
 
-Future<Tipoproducto?>crearTipoProductoController(
-    String tipoproducto2,
-    String descripcionProducto,
-    String isvTipoProducto,
-    context) async {
+Future<Tipoproducto?> crearTipoProductoController(String tipoproducto2,
+    String descripcionProducto, String isvTipoProducto, context) async {
   if (tipoproducto2.isNotEmpty &&
       descripcionProducto.isNotEmpty &&
       isvTipoProducto.isNotEmpty) {
-      List<Tipoproducto?> producto = await crearTipoProducto2(tipoproducto2, descripcionProducto, isvTipoProducto, context);
+    List<Tipoproducto?> producto = await crearTipoProducto2(
+        tipoproducto2, descripcionProducto, isvTipoProducto, context);
     if (producto != null) {
-       _ventanaExito(context);
+      _ventanaExito(context);
     } else {
       _ventanaError(context);
     }
@@ -61,7 +52,6 @@ Future<Tipoproducto?>crearTipoProductoController(
   }
 }
 
-
 Future<Tipoproducto?> actualizarTipoProductoController(
     String id,
     String tipoproducto2,
@@ -69,113 +59,111 @@ Future<Tipoproducto?> actualizarTipoProductoController(
     String isvTipoProducto,
     context) async {
   if (id.isNotEmpty &&
-  tipoproducto2.isNotEmpty &&
+      tipoproducto2.isNotEmpty &&
       descripcionProducto.isNotEmpty &&
-      isvTipoProducto.isNotEmpty
-      ) {
-    Tipoproducto? producto = await ActualizarTipoProducto(id, tipoproducto2, descripcionProducto, isvTipoProducto, context );
+      isvTipoProducto.isNotEmpty) {
+    Tipoproducto? producto = await ActualizarTipoProducto(
+        id, tipoproducto2, descripcionProducto, isvTipoProducto, context);
     if (producto != null) {
-       _ventanaExito(context);
+      _ventanaExito(context);
     } else {
-    _ventanaError(context);          
+      _ventanaError(context);
     }
     return producto;
   } else {
-    _ventanaError(context);          
+    _ventanaError(context);
   }
-
 }
 
 void _ventanaExito(BuildContext context) {
-    var idTipoProductoController = TextEditingController();
+  var idTipoProductoController = TextEditingController();
 
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          actions: <Widget>[
-            Container(
-              width: 500,
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(40),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Acción realizada con éxito.",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      SizedBox(
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        actions: <Widget>[
+          Container(
+            width: 500,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(40),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Acción realizada con éxito.",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                        width: 80,
                         height: 40,
-                      ),
-                      Container(
-                          width: 80,
-                          height: 40,
-                          margin: EdgeInsets.all(5),
-                          child: RaisedButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                              //initState();
-                            },
-                            child: Text('OK'),
-                            padding: EdgeInsets.all(10),
-                          )),
-                      SizedBox(
-                        height: 40,
-                      ),
-                    ]),
-              ),
+                        margin: EdgeInsets.all(5),
+                        child: RaisedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            //initState();
+                          },
+                          child: Text('OK'),
+                          padding: EdgeInsets.all(10),
+                        )),
+                    SizedBox(
+                      height: 40,
+                    ),
+                  ]),
             ),
-          ],
-        );
-      },
-    );
-  }
+          ),
+        ],
+      );
+    },
+  );
+}
 
-
-  void _ventanaError(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          actions: <Widget>[
-            Container(
-              width: 500,
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(40),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Ocurrio un error al realizar esta acción, intente de nuevo.",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      SizedBox(
+void _ventanaError(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        actions: <Widget>[
+          Container(
+            width: 500,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(40),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Ocurrio un error al realizar esta acción, intente de nuevo.",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Container(
+                        width: 80,
                         height: 40,
-                      ),
-                      Container(
-                          width: 80,
-                          height: 40,
-                          margin: EdgeInsets.all(5),
-                          child: RaisedButton(
-                            onPressed: () {
-                              Navigator.popAndPushNamed(
-                                  context, 'PantallaTipoProductos');
-                              //initState();
-                            },
-                            child: Text('OK'),
-                            padding: EdgeInsets.all(10),
-                          )),
-                      SizedBox(
-                        height: 40,
-                      ),
-                    ]),
-              ),
+                        margin: EdgeInsets.all(5),
+                        child: RaisedButton(
+                          onPressed: () {
+                            Navigator.popAndPushNamed(
+                                context, 'PantallaTipoProductos');
+                            //initState();
+                          },
+                          child: Text('OK'),
+                          padding: EdgeInsets.all(10),
+                        )),
+                    SizedBox(
+                      height: 40,
+                    ),
+                  ]),
             ),
-          ],
-        );
-      },
-    );
-  }
+          ),
+        ],
+      );
+    },
+  );
+}
